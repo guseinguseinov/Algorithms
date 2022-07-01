7676f4f6e25e4c11e0114d66db4507463eba4dd4
guseinguseinov <gusein.guseinov2002@gmail.com>
2022-07-01 12:05:25 +0100

    dublicates done

---
+++ containsDublicate.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var containsDuplicate = function(nums) {
+    let uniqueArr = [...new Set(nums)];
+    if (nums.length === uniqueArr.length) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+
+// RESULTS : 
+// 70 / 70 test cases passed.
+// Runtime: 90 ms
+// Memory Usage: 51 MB
