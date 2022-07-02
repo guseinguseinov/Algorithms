57c53015429d0a528d18ef6046bbff22a96b4691
guseinguseinov <gusein.guseinov2002@gmail.com>
2022-07-03 00:34:16 +0100

    yesss one more

---
+++ evalRPN.js
@@ -0,0 +1,38 @@
+var evalRPN = function(tokens) {
+    let arr = [];
+    for (let i = 0; i < tokens.length; i++) {
+        if (tokens[i] == "+") {
+          let newItem = arr[arr.length - 1] + arr[arr.length - 2];
+          arr.pop();
+          arr.pop();
+          arr.push(newItem);
+        }
+        else if (tokens[i] == "-") {
+          let newItem =  arr[arr.length - 2] - arr[arr.length - 1];
+          arr.pop();
+          arr.pop();
+          arr.push(newItem);
+        }
+        else if (tokens[i] == "*") {
+          let newItem = arr[arr.length - 1] * arr[arr.length - 2];
+          arr.pop();
+          arr.pop();
+          arr.push(newItem);
+        }
+        else if (tokens[i] == "/") {
+          let newItem = parseInt(arr[arr.length - 2]/arr[arr.length - 1]);
+          arr.pop();
+          arr.pop();
+          arr.push(Number(newItem));
+        }
+        else {
+          arr.push(Number(tokens[i]));
+        }
+    }
+    return arr[0];
+};
+
+// RESULTS :
+// 20 / 20 test cases passed.
+// Runtime: 98 ms
+// Memory Usage: 44.6 MB
