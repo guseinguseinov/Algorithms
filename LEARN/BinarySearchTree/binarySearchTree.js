17764e5e8b0bc2473362b07fe479a442fefbfc69
guseinguseinov <gusein.guseinov2002@gmail.com>
2022-09-19 10:33:12 +0100

    bfs done

--- LEARN/BinarySearchTree/binarySearchTree.js
+++ LEARN/BinarySearchTree/binarySearchTree.js
@@ -52,18 +52,30 @@ class BinarySearchTree {
             }
         }
     }
+
+    BFS() {
+        let queue = [];
+        let data = [];
+        let node = this.root;
+        queue.push(node);
+
+        while (queue.length) {
+            node = queue.shift();
+            data.push(node.val);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        return data;
+    }
 }
 
 const tree = new BinarySearchTree();
 
 tree.insert(10);
-tree.insert(5);
-tree.insert(13);
-tree.insert(11);
-tree.insert(2);
-tree.insert(16);
-tree.insert(7);
-
-console.log(tree.find(7));
-
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+console.log(tree.BFS());
 console.log(tree);
